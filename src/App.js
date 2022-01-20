64d71296a21d7bc56e6c807433aea5ea6b85820e
Tobias Wutz <tobiaswutz22@gmail.com>
2022-01-20 14:06:53 +0100

    tobias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme, ColorModeProvider, CSSReset, } from '@chakra-ui/react';
 import Navbar from './Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
--- src/Frontend.jsx
+++ src/Frontend.jsx
@@ -1,9 +1,32 @@
-function Frontend() {
-    return (
-      <div>
-        <p>Frontend</p>
-      </div>
-    );
-  }
-  
-  export default Frontend;
+import Card from './Card';
+import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+export default function Frontend() {
+  const [ data, setData ] = useState(null)
+  const [ loading, setLoading ] = useState(true)
+
+
+  useEffect(() => {
+      axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        })
+        .finally(() => {
+          setLoading(false);
+          console.log("fertig");
+          clearInterval();
+        });
+  }, []);
+
+  return (
+    <div className="home">
+      {data ? '' : <ClipLoader size={100} />}
+        {data && <Card data={data} />}
+    </div>
+  );
+}
