7e2199ef9607890c72dffefbbc716d1867e3d469
Mimoona <memoona.raheel8@gmail.com>
2022-01-20 13:02:39 +0100

    added heading in home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
   <div>
-      <p>Home</p>
+      {Navbar}
+      <h1>Roadmaps</h1>
+      <Link to="/frontend" onClick={}></Link>
   </div>
   );
 }
