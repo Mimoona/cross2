bfaae7e452f05aa9d3bde963eafb5ed2b7ff766d
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-20 15:07:10 +0100

    test

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,28 @@
+import Card from './Card';
+import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
 function Details() {
+  const [ data, setData ] = useState(null)
+
+
+  useEffect(() => {
+      axios("https://team-3-hackaton-server.herokuapp.com/api/all")
+        .then((response) => {
+          console.log(response.data)
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        })
+        .finally(() => {
+          console.log("fertig");
+          clearInterval();
+        });
+  }, []);
+
+
     return <div>Datails</div>;
   }
   
