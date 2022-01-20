09c82dd1a041a2aa9dcccc31ee57e4eac0a63013
jcwalina <joannacwalinaa@gmail.com>
2022-01-20 14:12:40 +0100

    footer made

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './Footer';
 
 function Home() {
   return (
   <div>
       <p>Home</p>
+      <Footer/>
   </div>
   );
 }
